30843862b34a065e68b17125a44fb16784f7f0a4
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-10 23:36:47 +0000

    Print a string to stdo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print a sentence with puts.
+ *
+ * Return: 0 (Always).
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	{
+		return (0);
+	}
+}
