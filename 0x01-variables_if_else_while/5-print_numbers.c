c6132262a294ebf6020949fe73495d5f823b862e
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-12 13:17:22 +0000

    Print all the digit numbers of decimal

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the digit numbers of base 10.
+ *
+ * Return: 0 (Always).
+ */
+int main(void)
+{
+	int n
+		;
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
