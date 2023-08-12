3e7846a5acb6615e9953172f8912b83050bd1fdc
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-12 21:01:13 +0000

    Print all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,22 @@ int main(void)
 {
 	int n1, n2, n3;
 
+	for (n1 = 48; n1 <= 55; n1++)
 	{
-		for (n1 = 48; n1 <= 55; n1++)
+		for (n2 = 49; n2 <= 56; n2++)
 		{
-			for (n2 = 49; n2 <= 56; n2++)
-			{
-				for (n3 = 50; n3 <= 57; n3++)
-					if (n3 > n2 && n2 > n1)
+			for (n3 = 50; n3 <= 57; n3++)
+				if (n3 > n2 && n2 > n1)
+				{
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
+					if (n1 != 55 || n2 != 56 || n3 != 57)
 					{
-						putchar(n1);
-						putchar(n2);
-						putchar(n3);
-						if (n1 != 55 || n2 != 56 || n3 != 57)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
-			}
+				}
 		}
 	}
 	putchar('\n');
