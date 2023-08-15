f08c678d0b6edda9d15955b782387f2da4f50166
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-15 16:34:01 -0700

    Print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_l.çhiast_digit - Prints the last digit.
+ * print_last_digit - Prints the last digit.
  * @r: Parameyer 1.
  * Return: The value of thr last digiit.
  */
@@ -9,7 +9,7 @@
 int print_last_digit(int r)
 {
 	int l = r % 10;
-	
+
 	if (l < 0)
 	{
 		l = l * -1;
