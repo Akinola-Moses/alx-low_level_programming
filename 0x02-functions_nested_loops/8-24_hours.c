88e6aea8d4ea83dfcedc2656407ed94427bca0f8
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-16 05:04:14 -0700

    Print every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of J.B.
+ * Return: 0.
+ */
+
+void jack_bauer(void)
+{
+	int hr = 0;
+	int mt = 0;
+
+	while (hr < 24)
+	{
+		while (mt < 60)
+		{
+			_putchar((mt / 10) + '0');
+			_putchar((mt % 10) + '0');
+			_putchar(':');
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar('\n');
+			mt++;
+		}
+		hr++;
+	}
+}
