cdaff3c3502bdacb27be2dae92faa8487622f339
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-15 21:47:27 -0700

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds twp numbers.
+ * @x: n1 to add.
+ * @y: n2 to add.
+ * Return: 0.
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
