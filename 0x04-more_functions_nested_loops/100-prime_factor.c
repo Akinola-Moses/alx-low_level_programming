d4f8a174d2e0e48cbd1ea655c8599acd171bc034
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-18 13:18:32 +0000

    print the largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of the number.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	long pn = 612852475143;
+	long pf = 2;
+
+	while (pf <= pn)
+	{
+		if ((pn % pf) == 0)
+		{
+			pn = pn / pf;
+		}
+		pf++;
+	}
+	printf("%ld", pn);
+	printf("\n");
+	return (0);
+}
