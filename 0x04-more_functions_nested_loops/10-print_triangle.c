a37fbab05a6f2bcaa2e210112ad59d8cfab00775
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-18 12:18:23 +0000

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,20 +10,19 @@ void print_triangle(int size)
 	int t1;
 	int t2;
 
-	if (t1 <= 0 size)
+	if (size <= 0)
 	{
 		_putchar('\n');
-		return (0);
 	}
 	for (t1 = 0; t1 < size; t1++)
 	{
-		for (t2 = 0; t2 < size t1 - 1; t2++)
+		for (t2 = 0; t2 < size - t1 - 1; t2++)
 		{
 			_putchar(' ');
 		}
 		for (t2 = 0; t2 < t1 + 1; t2++)
 		{
-			_putchar(35 + '0');
+			_putchar(35);
 		}
 		_putchar('\n');
 	}
