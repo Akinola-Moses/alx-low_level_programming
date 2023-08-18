f15a7c7458e35659c34206f0b5dca85a5fb8e442
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-18 04:54:08 -0700

    push to pull into local machine

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,22 +7,23 @@
  */
 void print_triangle(int size)
 {
-	int t1 = 0;
+	int t1;
 	int t2;
 
-	if (t1 < (size - 1))
+	if (t1 <= 0 size)
 	{
 		_putchar('\n');
+		return (0);
 	}
 	for (t1 = 0; t1 < size; t1++)
 	{
-		for (t2 = 0; t2 < (size - 1); t2++)
+		for (t2 = 0; t2 < size t1 - 1; t2++)
 		{
 			_putchar(' ');
 		}
-		for (t2--; t2 < size; t2++)
+		for (t2 = 0; t2 < t1 + 1; t2++)
 		{
-			_putchar(35);
+			_putchar(35 + '0');
 		}
 		_putchar('\n');
 	}
