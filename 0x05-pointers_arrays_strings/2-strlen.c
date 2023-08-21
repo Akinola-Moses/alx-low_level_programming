ccf206714131d9c6c1d1b00d26b7b0f9b13b0da4
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-21 16:08:35 +0000

    Return the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the lenght of a string.
+ * @s: String to return the lenght.
+ *
+ * Return: The lenght.
+ */
+
+int _strlen(char *s)
+{
+	int l; /*Declare an it to loop through thestring*/
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+
+	}
+	return (l);
+}
