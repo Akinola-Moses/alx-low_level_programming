4b02e0f09514fc9ed9adff8bf232ae8c68499f69
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-22 15:23:31 +0000

    Generate random password for program crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Generate random valids paasword.
+ *
+ * Return: 0.
+ * 
+ */
+
+int main(void)
+{
+	int l;
+	int rd = 0;
+	int sum;
+	time_t pt;
+
+
+	sum = 0;
+	srand((int) time(&pt));
+	
+	for (l = 0; sum < 2772; l++)
+	{
+		rd = rand() % 128;
+		if (sum + rd > 2772)
+		{
+			break;
+		}
+		sum = sum + rd;
+		printf("%c", rd);
+	}
+	printf("%c\n", 2772 - sum);
+	return (0);
+}
