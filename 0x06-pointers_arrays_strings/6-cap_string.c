b0ad9d4ab414af94d33f76f9ca2518384b7460bd
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-24 05:29:38 -0700

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,28 @@
 char *cap_string(char *s)
 {
 	int idx = 0;
-	int cl;
+	int cl = 1;
 
 	while (s[idx] != '\0')
 	{
-		if s[idx] == ',' || s[idx] == 
+		if (s[idx] == ' ' || s[idx] == '\t'
+|| s[idx] == '\n' || s[idx] == ',' || s[idx] == ';'
+|| s[idx] == '.' || s[idx] == '!' || s[idx] == '?'
+|| s[idx] == '"' || s[idx] == '(' || s[idx] == ')' ||
+s[idx] == '{' || s[idx] == '}')
+		{
+			cl = 1;
+		}
+		else if (cl && s[idx] >= 'a' && s[idx] <= 'z')
+		{
+			s[idx] = s[idx] -  'a' + 'A';
+			cl = 0;
+		}
+		else if (!cl && s[idx] >= 'A' && s[idx] <= 'Z')
+		{
+			s[idx] = s[idx] - 'A' + 'a';
+		}
+		idx++;
+	}
+	return (s);
+}
