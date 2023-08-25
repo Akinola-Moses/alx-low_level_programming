f97822a9a6bfc77251da662600c68e9e9ce228a9
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-25 04:29:36 +0000

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,30 +8,28 @@
 
 char *cap_string(char *s)
 {
-	char *p = s;
 	int idx = 0;
 	int cl = 1;
-
-	while (s[idx] != '\0')
 	{
 		if (s[idx] == ' ' || s[idx] == '\t'
-|| s[idx] == '\n' || s[idx] == ',' || s[idx] == ';'
-|| s[idx] == '.' || s[idx] == '!' || s[idx] == '?'
-|| s[idx] == '"' || s[idx] == '(' || s[idx] == ')' ||
-s[idx] == '{' || s[idx] == '}')
+				|| s[idx] == '\n' || s[idx] == ','
+				|| s[idx] == ';' || s[idx] == '.'
+				|| s[idx] == '!' || s[idx] == '?'
+				|| s[idx] == '"' || s[idx] == '('
+				|| s[idx] == ')' || s[idx] == '{'|| s[idx] == '}')
 		{
 			cl = 1;
 		}
 		else if (cl && s[idx] >= 'a' && s[idx] <= 'z')
 		{
-			s[idx] = s[idx] -  'a' + 'A';
+			s[idx] -= ('a' - 'A');
 			cl = 0;
 		}
 		else if (!cl && s[idx] >= 'A' && s[idx] <= 'Z')
 		{
-			s[idx] = s[idx] - 'A' + 'a';
+				s[idx] += ('a' - 'A');
 		}
 		idx++;
 	}
-	return (p);
+	return (s);
 }
