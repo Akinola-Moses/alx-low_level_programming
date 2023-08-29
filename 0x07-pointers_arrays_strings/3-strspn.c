10b774ddcc728e0d1046a4235695261a8fb9f96e
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-29 01:28:16 +0000

    Pull

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: string.
+ * @: Par 2.
+ *
+ * Return: n of byte.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int nm = 0;
+
+	
