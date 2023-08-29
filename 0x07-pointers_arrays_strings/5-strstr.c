2c082c84c8b91b245c7d2779bb5da9ff2a701917
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-29 17:11:29 +0000

    Locate a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strstr - Locates a substring needle.
+ * @haystack: str 1.
+ * @needle: str 2.
+ *
+ * Return: pointer or '\0'.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	for (; *haystack; haystack++)
+	{
+		char *a = haystack, *b = needle;
+		for (; *b && *a == *b; a++, b++)
+		{
+
+		}
+		if (*b == '\0')
+		{
+			return (haystack);
+		}
+	}
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	return ('\0');
+}
