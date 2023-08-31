02c4a108129d33c9819b2c06caed1d4da4517b40
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-08-31 02:47:14 +0000

    Print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prnt a string in reverse.
+ * @s: The string.
+ *
+ * Return: nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
