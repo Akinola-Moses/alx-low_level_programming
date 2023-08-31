b1867bfcde47bd3f42146f3617b86d306840f69b
Akinola Moses <akinolamosesmyself@gmail.com>
2023-08-30 22:00:37 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The num.
+ *
+ * Return: The sqrt.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return(my_sqrt(n, 1));
+}
+
+/**
+ * my_sqrt - Recursively find the sqrt.
+ * @n: The number.
+ * @idx: Finder.
+ *
+ * Return: sqrt, 0 or -1.
+ */
+
+int my_sqrt(n, idx)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (idx * idx > n)
+	{
+		return (-1);
+	}
+	else if (idx * idx == n)
+	{
+		return (idx);
+	else
+	{
+		return (my_sqrt(n, idx + 1));
+	}
+}
