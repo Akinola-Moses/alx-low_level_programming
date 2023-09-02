970f1d2d00383e70ac0abf306a839853f68aaf98
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-09-02 12:15:09 +0000

    Print all argument received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all arguement passed.
+ * @argc: Argument count.
+ * @argv: Arument vector(array).
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int idx;
+	
+	for (idx = 0; idx < argc; idx++)
+	{
+		printf("%s\n", argv[idx]);
+	}
+	return (0);
+}
