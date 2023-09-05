6db80326ee77ae4f9c436835a6c721f44bfd65be
Akinola Moses <akinolamosesmyself@gmail.com>
2023-09-05 13:02:38 -0700

    Copy a string into another dynamically allocating the memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,29 +14,28 @@ char *_strdup(char *str)
 	char *str1;
 	int idx = 0;
 	int l = 0;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (; str[l] != '\0'; l++)
 	{
-		
+
 	}
-	
+
 	str1 = malloc(sizeof(char) * (l + 1));
-	
+
 	if (str1 == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (; idx < l; idx++)
 	{
-		str1[idx]= str[idx];
+		str1[idx] = str[idx];
 	}
 	str1[idx] = '\0';
 	return (str1);
-	
 }
