30c0097faa5d9998759d4d156a477d3407c29494
Akinola Moses <akinolamosesmyself@gmail.com>
2023-09-11 16:14:15 -0700

    Define a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type.
+ * @name: The name.
+ * @age: The age.
+ * @owner: The owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
