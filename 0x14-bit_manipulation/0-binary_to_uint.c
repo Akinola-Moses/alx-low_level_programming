585917d6d7266bf19669ec3bfee6f712a13d3a31
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-09-29 03:16:48 +0000

    Convert binary to Usigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,4 +9,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int b_binary = 1;
+	unsigned int u_int = 0;
+
+
+	for (; b != NULL && *b != '\0'; b++)
+	{
+		if (*b == '0')
+		{
+
+		}
+		else if (*b == '1')
+		{
+			u_int = u_int | b_binary;
+		}
+		else
+		{
+			return (0);
+		}
+
+		b_binary = b_binary << 1;
+	}
+
+	return (u_int);
 }
