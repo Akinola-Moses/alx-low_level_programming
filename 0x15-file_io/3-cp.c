4173dc095c704f1d982415c3557051b378267ca2
Akinola-Moses <akinolamosesmyself@gmail.com>
2024-02-16 13:42:06 +0000

    Copy the content of a file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+void _printerr(int e_exit, const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	dprintf(STDERR_FILENO, format, args);
+
+	va_end(args);
+	exit(e_exit);
+}
+
+int main(int argc, char *argv[])
+{
+	char my_buf[MY_BUF];
+	int ffrom = 0, fto = 0;
+	int n_read, n_write;
+
+	if (argc != 3)
+	{
+		_printerr(97, "Usage: cp file_from file_to\n");
+	}
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom == -1)
+	{
+		_printerr(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+
+	fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fto == -1)
+	{
+		close(ffrom);
+		_printerr(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	n_read = read(ffrom, my_buf, MY_BUF);
+	while (n_read > 0)
+	{
+		n_write = write(fto, my_buf, n_read);
+		if (n_write != n_read)
+		{
+			close(ffrom);
+			close(fto);
+			_printerr(99, "Error: Can't write to %s\n", argv[2]);
+		}
+		n_read = read(ffrom, my_buf, MY_BUF);
+	}
+	if (n_read == -1)
+	{
+		close(ffrom);
+		close(fto);
+		_printerr(98, "Error: Can't read from file %s\n", argv[2]);
+	}
+	if (close(ffrom) == -1)
+	{
+		_printerr(100, "Error: Can't close fd %i\n", ffrom);
+	}
+
+	if (close(fto) == -1)
+	{
+		_printerr(100, "Error: Can't close fd %i\n", fto);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MY_BUF 1024
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _printerr(int e_exit, const char *format, ...);
 
 #endif
